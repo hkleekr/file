d6fa7e98bb80bfcda756b6acd753b418a23edd43
Hyekyung <mwjer333@gmail.com>
2023-06-05 11:45:55 +0900

    file/upload

---
+++ src/main/java/file/fileupload/controller/FileController.java
@@ -0,0 +1,38 @@
+package file.fileupload.controller;
+
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+@RequestMapping("/files")
+public class FileController {
+
+    private String fileDir;
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFile(@RequestParam String fileName,
+                           @RequestParam MultipartFile file) throws IOException {
+
+        if (!file.isEmpty()) {
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
+        }
+        return "upload-form";
+    }
+}
